d486e013918565df2b3846c98b9edfc32fcebefa
jeffer <jeffersunde72@gmail.com>
2022-03-28 05:19:52 +0200

    Learning django

--- lecture3/hello/views.py
+++ lecture3/hello/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello!")
+    return render(request, "hello/index.html")
 
 
 def jeffer(request):
@@ -15,4 +15,6 @@ def david(request):
 
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
